68fea19524b03c83a18fa0c70f8b246a199f62cc
Carles Mitjans <carles.mitjans.coma@gmail.com>
2025-01-24 09:04:43 +0100

    feat: register entities

---
+++ src/any_vec.rs
@@ -0,0 +1,113 @@
+use std::{
+    alloc::{alloc, handle_alloc_error, realloc, Layout},
+    mem::ManuallyDrop,
+    ptr::{copy_nonoverlapping, read, NonNull},
+};
+
+#[derive(Debug)]
+pub struct AnyVec {
+    ptr: NonNull<u8>,
+    layout: Layout,
+    len: usize,
+    cap: usize,
+}
+
+impl AnyVec {
+    pub fn new(layout: Layout) -> Self {
+        Self {
+            ptr: NonNull::dangling(),
+            len: 0,
+            cap: 0,
+            layout,
+        }
+    }
+
+    fn grow(&mut self) {
+        let (new_cap, new_layout) = if self.cap == 0 {
+            (1, self.layout)
+        } else {
+            (
+                self.cap * 2,
+                Layout::from_size_align(self.layout.size() * self.cap * 2, self.layout.align())
+                    .unwrap(),
+            )
+        };
+
+        assert!(
+            new_layout.size() <= isize::MAX as usize,
+            "Allocation too large"
+        );
+
+        let new_ptr = if self.cap == 0 {
+            unsafe { alloc(new_layout) }
+        } else {
+            let old_layout =
+                Layout::from_size_align(self.layout.size() * self.cap, self.layout.align())
+                    .unwrap();
+            unsafe { realloc(self.ptr.as_ptr(), old_layout, new_layout.size()) }
+        };
+
+        self.cap = new_cap;
+        self.ptr = match NonNull::new(new_ptr) {
+            Some(ptr) => ptr,
+            None => handle_alloc_error(new_layout),
+        }
+    }
+
+    pub fn push<T>(&mut self, element: T) {
+        if self.len == self.cap {
+            self.grow();
+        }
+
+        let ptr = ManuallyDrop::new(element);
+        let src = NonNull::from(&*ptr).as_ptr() as *const u8;
+
+        let dst = unsafe { self.ptr.as_ptr().add(self.len * self.layout.size()) };
+
+        unsafe {
+            copy_nonoverlapping(src, dst, self.layout.size());
+        };
+
+        self.len += 1;
+    }
+
+    pub fn pop<T>(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None;
+        }
+
+        self.len -= 1;
+        let src = unsafe { self.ptr.as_ptr().add(self.len * self.layout.size()) as *const T };
+        Some(unsafe { read(src) })
+    }
+
+    pub fn get<T>(&self, index: usize) -> Option<&T> {
+        if index >= self.len {
+            return None;
+        }
+
+        let ptr = unsafe { self.ptr.as_ptr().add(index * self.layout.size()) };
+        let ptr = ptr.cast::<T>();
+
+        Some(unsafe { &*ptr })
+    }
+
+    pub fn get_mut<T>(&mut self, index: usize) -> Option<&mut T> {
+        if index >= self.len {
+            return None;
+        }
+
+        let ptr = unsafe { self.ptr.as_ptr().add(index * self.layout.size()) };
+        let ptr = ptr.cast::<T>();
+
+        Some(unsafe { &mut *ptr })
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn layout(&self) -> Layout {
+        self.layout
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,97 @@
+mod any_vec;
+
+use any_vec::AnyVec;
 use std::{
+    alloc::Layout,
     any::{Any, TypeId},
+    collections::BTreeSet,
     collections::HashMap,
 };
 
 type ArchetypeMap = HashMap<ArchetypeId, usize>;
 
-type Component = Box<dyn Any>;
-type ComponentType = Vec<ComponentId>;
+type ComponentType = BTreeSet<ComponentId>;
 
 type EntityId = usize;
 type ComponentId = TypeId;
 type ArchetypeId = usize;
 
+pub struct Column {
+    components: AnyVec,
+}
+
 pub struct Archetype {
-    components: Vec<Component>,
+    columns: Vec<Column>,
+}
+
+#[derive(Clone, Copy)]
+pub struct EntityRecord {
+    pub id: EntityId,
+    pub archetype_id: ArchetypeId,
+    pub row: usize,
+}
+
+pub struct EntityCreator<'a> {
+    world: &'a mut World,
+    archetype: Archetype,
+    components_set: ComponentType,
+}
+
+impl EntityCreator<'_> {
+    pub fn with_component<T: Any>(mut self, component: T) -> Self {
+        let mut any_vec = AnyVec::new(Layout::array::<T>(1).unwrap());
+        any_vec.push(component);
+
+        self.archetype.columns.push(Column {
+            components: any_vec,
+        });
+
+        let component_id = TypeId::of::<T>();
+        self.components_set.insert(component_id);
+
+        self.world
+            .component_index
+            .entry(component_id)
+            .or_default()
+            .insert(
+                self.world.archetypes.len(),
+                self.archetype.columns.len() - 1,
+            );
+        self
+    }
+
+    pub fn finish(self) -> EntityRecord {
+        let entity_id = self.world.entity_index.len();
+        let entity_record = EntityRecord {
+            id: entity_id,
+            archetype_id: self.world.archetypes.len(),
+            row: 0,
+        };
+
+        self.world.entity_index.insert(entity_id, entity_record);
+
+        self.world
+            .archetype_index
+            .insert(self.components_set, self.world.archetypes.len());
+
+        self.world.archetypes.push(self.archetype);
+
+        entity_record
+    }
 }
 
 #[derive(Default)]
 pub struct World {
     archetypes: Vec<Archetype>,
 
-    entity_index: HashMap<EntityId, ArchetypeId>,
+    entity_index: HashMap<EntityId, EntityRecord>,
     archetype_index: HashMap<ComponentType, ArchetypeId>,
     component_index: HashMap<ComponentId, ArchetypeMap>,
 }
 
 impl World {
-    pub fn register_component<T: Any>(&mut self) {
-        let component_id = TypeId::of::<T>();
-        let archetype_id = self.archetypes.len();
-
-        self.archetypes.push(Archetype { components: vec![] });
-
-        self.archetype_index
-            .insert(vec![component_id], archetype_id);
-    }
-
     pub fn has_component<T: Any>(&self, entity: EntityId) -> bool {
-        let Some(archetype_id) = self.entity_index.get(&entity) else {
+        let Some(entity_record) = self.entity_index.get(&entity) else {
             return false;
         };
 
@@ -45,17 +99,43 @@ impl World {
             return false;
         };
 
-        archetype_map.contains_key(archetype_id)
+        archetype_map.contains_key(&entity_record.archetype_id)
     }
 
     pub fn get_component<T: Any>(&self, entity: EntityId) -> Option<&T> {
-        let archetype_id = self.entity_index.get(&entity)?;
-        let archetype = self.archetypes.get(*archetype_id)?;
+        let entity_record = self.entity_index.get(&entity)?;
+        let archetype = self.archetypes.get(entity_record.archetype_id)?;
 
         let archetype_map = self.component_index.get(&TypeId::of::<T>())?;
-        let column_id = archetype_map.get(archetype_id)?;
+        let column_id = archetype_map.get(&entity_record.archetype_id)?;
 
-        archetype.components.get(*column_id)?.downcast_ref::<T>()
+        archetype
+            .columns
+            .get(*column_id)?
+            .components
+            .get::<T>(entity_record.row)
+    }
+
+    pub fn get_component_mut<T: Any>(&mut self, entity: EntityId) -> Option<&mut T> {
+        let entity_record = self.entity_index.get(&entity)?;
+        let archetype = self.archetypes.get_mut(entity_record.archetype_id)?;
+
+        let archetype_map = self.component_index.get(&TypeId::of::<T>())?;
+        let column_id = archetype_map.get(&entity_record.archetype_id)?;
+
+        archetype
+            .columns
+            .get_mut(*column_id)?
+            .components
+            .get_mut::<T>(entity_record.row)
+    }
+
+    pub fn spawn(&mut self) -> EntityCreator {
+        EntityCreator {
+            world: self,
+            components_set: BTreeSet::new(),
+            archetype: Archetype { columns: vec![] },
+        }
     }
 }
 
@@ -63,19 +143,79 @@ impl World {
 mod tests {
     use super::*;
 
-    struct Health;
+    struct Health(u32);
+    #[derive(Debug)]
+    struct Name(String);
 
     #[test]
-    fn it_registers_components() {
+    fn spawn_entity_with_single_component() {
         let mut world = World::default();
-        let component_id = TypeId::of::<Health>();
 
-        world.register_component::<Health>();
+        let entity_record = world.spawn().with_component(Health(150)).finish();
 
-        assert_eq!(world.archetype_index.len(), 1);
+        assert_eq!(world.archetypes.len(), 1);
+        assert_eq!(entity_record.archetype_id, 0);
+        assert_eq!(entity_record.row, 0);
 
-        let archetype_id = world.archetype_index.get(&vec![component_id]).unwrap();
+        let archetype_set = world.component_index.get(&TypeId::of::<Health>()).unwrap();
+        assert_eq!(archetype_set.len(), 1);
+
+        let column_id = archetype_set.get(&entity_record.archetype_id).unwrap();
+        assert_eq!(*column_id, 0);
+
+        let health = world.archetypes[entity_record.archetype_id].columns[*column_id]
+            .components
+            .get::<Health>(entity_record.row);
+
+        assert_eq!(health.unwrap().0, 150);
+    }
+
+    #[test]
+    fn spawn_entity_with_multiple_components() {
+        let mut world = World::default();
+
+        let entity_record = world
+            .spawn()
+            .with_component(Health(40))
+            .with_component(Name(String::from("Carles")))
+            .finish();
+
+        assert_eq!(world.archetypes.len(), 1);
+        assert_eq!(entity_record.archetype_id, 0);
+        assert_eq!(entity_record.row, 0);
+
+        let name_id = TypeId::of::<Name>();
+        let health_id = TypeId::of::<Health>();
+        let components_set = BTreeSet::from([name_id, health_id]);
+        let archetype_id = world.archetype_index.get(&components_set).unwrap();
 
         assert_eq!(*archetype_id, 0);
+
+        let archetype = world.archetypes.get(*archetype_id).unwrap();
+
+        assert_eq!(archetype.columns.len(), 2);
+
+        let healths = &archetype.columns.first().unwrap().components;
+        let names = &archetype.columns.last().unwrap().components;
+
+        assert_eq!(healths.len(), 1);
+        assert_eq!(healths.len(), names.len());
+
+        let health = healths.get::<Health>(0).unwrap();
+        let name = names.get::<Name>(0).unwrap();
+
+        assert_eq!(health.0, 40);
+        assert_eq!(name.0, "Carles");
+
+        let health = world.get_component::<Health>(entity_record.id).unwrap();
+        let name = world.get_component::<Name>(entity_record.id).unwrap();
+        assert_eq!(health.0, 40);
+        assert_eq!(name.0, "Carles");
+
+        let name = world.get_component_mut::<Name>(entity_record.id).unwrap();
+        name.0 = "Queco".to_owned();
+
+        let name = world.get_component::<Name>(entity_record.id).unwrap();
+        assert_eq!(name.0, "Queco");
     }
 }
